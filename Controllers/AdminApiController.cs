ab54ac39d2d97ca6c90a51251ea78cb016236465
davislatiss <davis.latiss@gmail.com>
2021-06-08 16:50:55 +0300

    project added
    2 tests passed

---
+++ Controllers/AdminApiController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using FlightPlanner.Attributes;
+
+namespace FlightPlanner.Controllers
+{
+    public class AdminApiController : ApiController
+    {
+        [Route ("admin-api/flights/{id}")]
+        [BasicAuthentication]
+        public IEnumerable<string> GetFlights(int id)
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ Controllers/TestingApiController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FlightPlanner.Controllers
+{
+    public class TestingApiController : ApiController
+    {
+        [Route ("Testing-api/clear")]
+        [HttpPost]
+        public IHttpActionResult Clear ()
+        {
+            return Ok();
+        }
+    }
+}
